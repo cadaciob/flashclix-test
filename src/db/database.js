366cc5b6fda0fea171e943071ad1359dab47a8cb
roadtodiploma <roadtodiploma@roadtodiplomas-MacBook-Pro.local>
2020-04-10 02:04:30 +0200

    2nd commit

--- src/db/database.js
+++ src/db/database.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose')
 // mongodb://127.0.0.1:27017/flashclix-api
 // mongodb+srv://htlwienwesterUser:<password>@cluster0-6kjju.mongodb.net/flashclix-api?retryWrites=true&w=majority
 
-mongoose.connect('mongodb+srv://htlwienwesterUser:htlwienwestPass@cluster0-6kjju.mongodb.net/flashclix-api?retryWrites=true&w=majority', {
+const db = process.env.MONGODB_URL
+
+mongoose.connect(db , {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
     useCreateIndex: true,
